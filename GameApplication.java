f47cc6b79298de0317dcde9e02aecaacc2bd5071
SHELLEYJ32 <44713951+SHELLEYJ32@users.noreply.github.com>
2018-11-03 08:41:05 -0400

    Update GameApplication.java

--- GameApplication.java
+++ GameApplication.java
@@ -1,23 +1,25 @@
 import javax.swing.JFrame;
+
 /**
+ * Main method for LearningGame
+ * 
  * @author BluePuddleCats
  * @version 11.2.18
- * main method for LearningGame
  */
 public class GameApplication {
 	/**
-	 * create the JFrame in which the game will be played
-   * initiate game play by calling gameController
+	 * create the JFrame in which the game will be played initiate game play by
+	 * calling GameController
 	 */
-	public static void main( String[] args ) {
+	public static void main(String[] args) {
 		// create a new JFrame to display the game
-		JFrame gameFrame = new JFrame("Game Frame");
-		
-		// set size
-		gameFrame.setSize( 800, 800 );
+		JFrame gameFrame = new JFrame("BluePuddleCat Education Game");
 
-		//call gameController to play game
-    gameFrame.add( new GameController());
+		// set size
+		gameFrame.setSize(600, 600);
+
+		// call gameController to play game
+		gameFrame.add(new GameController());
 
 		// exit normally on closing the window
 		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
