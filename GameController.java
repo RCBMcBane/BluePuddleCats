97e11d3464734a37952ca0878ca057e1d57e810c
SHELLEYJ32 <44713951+SHELLEYJ32@users.noreply.github.com>
2018-11-03 10:21:17 -0400

    Update GameController.java

--- GameController.java
+++ GameController.java
@@ -19,6 +19,7 @@ import javax.swing.JLabel;
 public class GameController extends JPanel implements ActionListener {
 	private GameLogic logic;
 	private boolean gameStart;
+	private String version;
 	private JButton catgry1, catgry2, catgry3;
 	private boolean correct;
 
@@ -28,10 +29,24 @@ public class GameController extends JPanel implements ActionListener {
 	public GameController() {
 		super(new BorderLayout());
 		setFocusable(true);
-		logic = new GameLogic();
-		logic.updateWord();
 		gameStart = false;
-		createView();
+		displayVersion();
+	}
+
+	/**
+	 * display two buttons for word and recycling game
+	 */
+	private void displayVersion() {
+		JPanel versionPane = new JPanel(new GridLayout(1, 2));
+		// create buttons
+		JButton wordVersion = new JButton("Word Game");
+		wordVersion.addActionListener(this);
+		JButton recycleVersion = new JButton("Recycle Game");
+		recycleVersion.addActionListener(this);
+		// add them to panels
+		versionPane.add(wordVersion);
+		versionPane.add(recycleVersion);
+		add(versionPane, BorderLayout.CENTER);
 	}
 
 	/**
@@ -40,9 +55,8 @@ public class GameController extends JPanel implements ActionListener {
 	 */
 	private void createView() {
 		displayCategoryScore();
-		displayWord();
-		// display instruction
-		displayComment(gameStart);
+		displayWordComment();
+		displaySkipEnd();
 	}
 
 	/**
@@ -50,19 +64,26 @@ public class GameController extends JPanel implements ActionListener {
 	 * categories for the word
 	 */
 	private void displayCategoryScore() {
+
 		JPanel categoryScorePane = new JPanel(new GridLayout(2, 1));
-		//create category view
+		// create category view
 		JPanel catPanel = new JPanel(new GridLayout(1, 3));
-		catgry1 = new JButton("Noun");
+		if (version.equals("Word Game")) {
+			catgry1 = new JButton("Noun");
+			catgry2 = new JButton("Verb");
+			catgry3 = new JButton("Adjective");
+		} else if (version.equals("Recycle Game")) {
+			catgry1 = new JButton("Trash");
+			catgry2 = new JButton("Recycle");
+			catgry3 = new JButton("Compost");
+		}
 		catgry1.addActionListener(this);
-		catgry2 = new JButton("Verb");
 		catgry2.addActionListener(this);
-		catgry3 = new JButton("Adjective");
 		catgry3.addActionListener(this);
 		catPanel.add(catgry1);
 		catPanel.add(catgry2);
 		catPanel.add(catgry3);
-		//create score view
+		// create score view
 		String score = "Score: " + String.valueOf(logic.getScore());
 		JLabel scoreLabel = new JLabel(score, SwingConstants.CENTER);
 		add(categoryScorePane, BorderLayout.NORTH);
@@ -72,57 +93,64 @@ public class GameController extends JPanel implements ActionListener {
 	}
 
 	/**
-	 * this method displays the new word for the user to guess
+	 * this method displays the new word for the user to guess and the comment
 	 */
-	private void displayWord() {
-		JPanel wordPane = new JPanel(new BorderLayout());
+	private void displayWordComment() {
+		// display word
+		JPanel wordCommentPane = new JPanel(new GridLayout(2, 1));
 		JLabel wordLabel = new JLabel(logic.getWord().getData(), SwingConstants.CENTER);
-		wordPane.add(wordLabel,BorderLayout.CENTER);
-		add(wordPane, BorderLayout.CENTER);
-		revalidate();
-	}
+		wordCommentPane.add(wordLabel);
 
-	/**
-	 * @param wasCorrect,
-	 *            boolean, true if the answer was correct this method displays the
-	 *            comments after the user has guessed they are different if the used
-	 *            got the answer right or wrong
-	 */
-	private void displayComment(boolean gameOn) {
+		// display comment or instructions
 		JPanel commentPane = new JPanel(new BorderLayout());
-		if (gameOn) {
-			//after user has clicked
-			//display comment
-			String comment = getComment(correct);
+		if (gameStart) {
+			// after user has clicked
+			// display comment
+			String comment = "";
+			if (correct) {
+				comment = "Great Job! You got it right!";
+			} else {
+				comment = "Good try, but that is not the right answer.";
+			}
 			JLabel commentLabel = new JLabel(comment, SwingConstants.CENTER);
 			commentPane.add(commentLabel, BorderLayout.CENTER);
 		} else {
-			//at the beginning on the game, before any clicks
-			//display instructions
-			String instructions = "<html>How to Play: <br/> A word will appear on the screen.<br/>You must decide if it is a noun, a verb, "
-					+ "or an adjective.<br/>When you have decided, click the corresponding button at the top.<br/>"
+			// at the beginning on the game, before any clicks
+			// display instructions
+			String instructions = "<html>How to Play: <br/> A word will appear on the screen.<br/>You must decide which "
+					+ "category it belongs to.<br/>When you have decided, click the corresponding button at the top.<br/>"
 					+ "If you get it right, you will get a point!<br/>Once you have clicked, a new word will appear."
 					+ "<br/>Try to get through all the words with the highest score possible!</html>";
 			JLabel instructionLabel = new JLabel(instructions, SwingConstants.CENTER);
 			commentPane.add(instructionLabel, BorderLayout.CENTER);
 		}
-		add(commentPane, BorderLayout.SOUTH);
+		wordCommentPane.add(commentPane);
+		add(wordCommentPane, BorderLayout.CENTER);
 		revalidate();
 	}
 
 	/**
-	 * @param wasCorrect,
-	 *            boolean, true if the user's guess was correct returns a comment
-	 *            based on whether the user answer was correct or incorrect
+	 * display skip and end button
 	 */
-	private String getComment(boolean right) {
-		if (right) {
-			return "Great Job! You got it right!";
-		} else {
-			return "Good try, but that is not the right answer.";
-		}
+	private void displaySkipEnd() {
+		JPanel skipEndPane = new JPanel(new GridLayout(1, 2));
+		JButton skip = new JButton("Skip");
+		skip.addActionListener(this);
+		JButton end = new JButton("End");
+		end.addActionListener(this);
+		skipEndPane.add(skip);
+		skipEndPane.add(end);
+		add(skipEndPane,BorderLayout.SOUTH);
 	}
 
+	private void endGame() {
+		removeAll();
+		String finalScore = "Your Final Score is " + logic.getScore() + " / " + logic.getTimesAttempted();
+		JLabel finalScoreLabel = new JLabel(finalScore,  SwingConstants.CENTER);
+		add(finalScoreLabel, BorderLayout.CENTER);
+		revalidate();
+	}
+	
 	/**
 	 * this method performs the main game play of the Language Game
 	 * 
@@ -130,10 +158,34 @@ public class GameController extends JPanel implements ActionListener {
 	 */
 	public void actionPerformed(ActionEvent e) {
 		String text = ((AbstractButton) e.getSource()).getText();
+		// display different versions of game
+		if (text.equals("Word Game")) {
+			version = "Word Game";
+			logic = new GameLogic(version);
+			logic.updateWord();
+			removeAll();
+			createView();
+		} else if (text.equals("Recycle Game")) {
+			version = "Recycle Game";
+			logic = new GameLogic(version);
+			logic.updateWord();
+			removeAll();
+			createView();
+		}
+		else if (text.equals("Skip")) {
+			logic.updateWord();
+			createView();
+		}
+		else if (text.equals("End")) {
+			endGame();
+		}
 		// compare selected category with the original
-		correct = logic.compare(text);
-		logic.updateWord();
-		gameStart = true;
-		// refresh display
-		createView();
+		else {
+			correct = logic.compare(text);
+			logic.updateWord();
+			gameStart = true;
+			// refresh display
+			createView();
+		}
 	}
+}
