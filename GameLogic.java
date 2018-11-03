dc93847066414652e5d23978718d4d157a65fe6a
SHELLEYJ32 <44713951+SHELLEYJ32@users.noreply.github.com>
2018-11-03 10:21:59 -0400

    Update GameLogic.java

--- GameLogic.java
+++ GameLogic.java
@@ -1,30 +1,28 @@
-
-
 /**
- * Creates an instance of WordLibrary and generates
- * random words from it. The category the user chooses in the game
- * is compared to the category belonging to the word, and whether the 
- * user's choice is correct or not is returned. If the user's choice is correct,
- * the score is updated.
+ * Creates an instance of WordLibrary and generates random words from it. The
+ * category the user chooses in the game is compared to the category belonging
+ * to the word, and whether the user's choice is correct or not is returned. If
+ * the user's choice is correct, the score is updated.
  * 
  * @author BluePuddleCat
- * @version 11/2/18
- * Written for HackHolyoke 2018
+ * @version 11/2/18 Written for HackHolyoke 2018
  *
  */
 public class GameLogic {
-	
-	//global variables
-	private WordLibrary library;
+
+	// global variables
+	private Library library;
 	private Word word;
 	private int score;
-	
+	private int timesAttempted;
+
 	/**
 	 * constructor initiates new instance of WordLibrary, sets score to 0.
 	 */
-	public GameLogic() {
-		library = new WordLibrary();
+	public GameLogic(String version) {
+		library = new Library(version);
 		score = 0;
+		timesAttempted = 0;
 	}
 
 	/**
@@ -37,36 +35,50 @@ public class GameLogic {
 
 	/**
 	 * gets random word from WordLibrary
+	 * 
 	 * @return new word from WordLibrary
 	 */
 	public Word updateWord() {
 		word = library.getRandomWord();
 		return word;
 	}
-	
 
 	/**
-	 * 
+	 * get score
 	 * @return score
 	 */
 	public int getScore() {
 		return score;
 	}
-	
-	
+
 	/**
-	 * compare given category with the original category (string comparison, use “.equals”)
-	 * if the same, correct is true, update score by 1
-	 * if not, correct is false, do nothing
-	 * @param Category the category the user chose in the game for the word
+	 * reset score to 0
+	 */
+	public void resetScore() {
+		score = 0;
+	}
+
+	/**
+	 * get attempted times
+	 * @return attempted times
+	 */
+	public int getTimesAttempted() {
+		return timesAttempted;
+	}
+
+	/**
+	 * compare given category with the original category
+	 * 
+	 * @param Category
+	 *            the category the user chose in the game for the word
 	 * @return whether that word belongs in the category or not
 	 */
 	public boolean compare(String Category) {
 		String category = word.getCategory();
-		boolean correct = category.equals( Category );	
-		if( correct ) {
-			score ++;
-		} 
+		boolean correct = category.equals(Category);
+		if (correct)
+			score++;
+		timesAttempted++;
 		return correct;
 	}
 
