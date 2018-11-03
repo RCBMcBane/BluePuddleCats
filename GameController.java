ac292b3310a071192d8cd2ce47e17ecf560e38cd
SHELLEYJ32 <44713951+SHELLEYJ32@users.noreply.github.com>
2018-11-03 13:58:30 -0400

    Add files via upload

---
+++ GameController.java
@@ -0,0 +1,293 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Font;
+
+import javax.swing.ImageIcon;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.AbstractButton;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.JLabel;
+import javax.swing.JFrame;
+
+/**
+ * GameController class initiates game and display in GUI
+ * 
+ * @author BluePuddleCats
+ * @version 11/3/18
+ *
+ */
+public class GameController extends JPanel implements ActionListener {
+	private GameLogic logic;
+	private boolean gameStart;
+	private String version;
+	private JButton catgry1, catgry2, catgry3;
+	private boolean correct;
+	private Font font = new Font("Comic Sans MS", Font.PLAIN, 24);
+	private Font buttonFont = new Font("Comic Sans MS", Font.PLAIN, 20);
+	private Font wordFont = new Font("Comic Sans MS", Font.BOLD, 30);
+	private Font commentFont = new Font("Comic Sans MS", Font.PLAIN, 24);
+	private Font instructionFont = new Font("Comic Sans MS", Font.PLAIN, 14);
+	private Color background = new Color(175, 206, 250);
+	private Color red = new Color(255, 200, 200);
+	private Color green = new Color(172, 236, 169);
+	private Color yellow = new Color(235, 235, 194);
+
+	/**
+	 * constructor
+	 */
+	public GameController() {
+		super(new BorderLayout());
+		setFocusable(true);
+		gameStart = false;
+		setBackground(background);
+		try {
+			UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		displayVersion();
+	}
+
+	/**
+	 * display two buttons for word and recycling game
+	 */
+	private void displayVersion() {
+		JPanel versionPane = new JPanel(new GridLayout(1, 2));
+		// create buttons
+		JButton wordVersion = new JButton("Word Game");
+		wordVersion.setFont(font);
+		wordVersion.setBackground(red);
+		wordVersion.addActionListener(this);
+		JButton recycleVersion = new JButton("Recycle Game");
+		recycleVersion.setFont(font);
+		recycleVersion.setBackground(green);
+		recycleVersion.addActionListener(this);
+		// add them to panels
+		versionPane.add(wordVersion);
+		versionPane.add(recycleVersion);
+		add(versionPane, BorderLayout.CENTER);
+		revalidate();
+	}
+
+	/**
+	 * createView method creates all the elements that need to go into the frame for
+	 * the game to run.
+	 */
+	private void createView() {
+		displayCategoryScore();
+		displayWordComment();
+		displaySkipEnd();
+	}
+
+	/**
+	 * createCategoryView method creates the three buttons that are the possible
+	 * categories for the word
+	 */
+	private void displayCategoryScore() {
+		// Font font = new Font("Comic Sans MS", Font.BOLD, 30);
+
+		JPanel categoryScorePane = new JPanel(new GridLayout(2, 1));
+		categoryScorePane.setBackground(background);
+		// create category view
+		JPanel catPanel = new JPanel(new GridLayout(1, 3));
+		if (version.equals("Word Game")) {
+			catgry1 = new JButton("Noun");
+			catgry2 = new JButton("Verb");
+			catgry3 = new JButton("Adjective");
+		} else if (version.equals("Recycle Game")) {
+			catgry1 = new JButton("Trash");
+			catgry2 = new JButton("Recycle");
+			catgry3 = new JButton("Compost");
+		}
+		catgry1.setBackground(red);
+		catgry2.setBackground(green);
+		catgry3.setBackground(yellow);
+		catgry1.setFont(buttonFont);
+		catgry2.setFont(buttonFont);
+		catgry3.setFont(buttonFont);
+		catgry1.addActionListener(this);
+		catgry2.addActionListener(this);
+		catgry3.addActionListener(this);
+		catPanel.add(catgry1);
+		catPanel.add(catgry2);
+		catPanel.add(catgry3);
+		// create score view
+		String score = "Score: " + String.valueOf(logic.getScore());
+		JLabel scoreLabel = new JLabel(score, SwingConstants.CENTER);
+		scoreLabel.setFont(font);
+		add(categoryScorePane, BorderLayout.NORTH);
+		categoryScorePane.add(catPanel);
+		categoryScorePane.add(scoreLabel);
+		revalidate();
+	}
+
+	/**
+	 * this method displays the new word for the user to guess and the comment
+	 */
+	private void displayWordComment() {
+		// display word
+		JPanel wordCommentPane = new JPanel(new GridLayout(2, 1));
+		wordCommentPane.setBackground(background);
+		JLabel wordLabel = new JLabel(logic.getWord().getData(), SwingConstants.CENTER);
+		wordLabel.setFont(wordFont);
+		wordCommentPane.add(wordLabel);
+
+		// display comment or instructions
+		JPanel commentPane = new JPanel(new BorderLayout());
+		commentPane.setBackground(background);
+		if (gameStart) {
+			// after user has clicked
+			// display comment
+			String comment = "";
+			if (correct) {
+				comment = "Great Job! You got it right!";
+			} else {
+				comment = "Good try, but that is not the right answer.";
+			}
+			JLabel commentLabel = new JLabel(comment, SwingConstants.CENTER);
+			commentLabel.setFont(commentFont);
+			commentPane.add(commentLabel, BorderLayout.CENTER);
+		} else {
+			// at the beginning on the game, before any clicks
+			// display instructions
+			String instructions = "<html>How to Play: <br/> A word will appear on the screen.<br/>You must decide which "
+					+ "category it belongs to.<br/>When you have decided, click the corresponding button at the top.<br/>"
+					+ "If you get it right, you will get a point!<br/>Once you have clicked, a new word will appear."
+					+ "<br/>Try to get through all the words with the highest score possible!</html>";
+			JLabel instructionLabel = new JLabel(instructions, SwingConstants.CENTER);
+			instructionLabel.setFont(instructionFont);
+			commentPane.add(instructionLabel, BorderLayout.CENTER);
+		}
+		wordCommentPane.add(commentPane);
+		add(wordCommentPane, BorderLayout.CENTER);
+		revalidate();
+	}
+
+	/**
+	 * display skip and end button
+	 */
+	private void displaySkipEnd() {
+		JPanel skipEndPane = new JPanel(new GridLayout(1, 2));
+		// skip button
+		JButton skip = new JButton("Skip");
+		skip.setFont(font);
+		skip.setBackground(new Color(235, 235, 194));
+		skip.addActionListener(this);
+		// end button
+		JButton end = new JButton("End");
+		end.setFont(font);
+		end.setBackground(green);
+		end.addActionListener(this);
+
+		skipEndPane.add(skip);
+		skipEndPane.add(end);
+		add(skipEndPane, BorderLayout.SOUTH);
+	}
+
+	/**
+	 * end game and display final score
+	 * 
+	 * @return true if score is more than half attempted names
+	 */
+	private boolean endGame() {
+		removeAll();
+		String finalScore = "Your Final Score is " + logic.getScore() + " / " + logic.getTimesAttempted();
+		JLabel finalScoreLabel = new JLabel(finalScore, SwingConstants.CENTER);
+		finalScoreLabel.setFont(commentFont);
+		add(finalScoreLabel, BorderLayout.CENTER);
+		JButton restart = new JButton("Restart");
+		restart.addActionListener(this);
+		restart.setFont(buttonFont);
+		restart.setBackground(yellow);
+		add(restart, BorderLayout.SOUTH);
+		revalidate();
+		return (logic.getScore() >= logic.getTimesAttempted() / 2);
+	}
+
+	/**
+	 * play sound effect for buttons
+	 */
+	private void sound(String kind) {
+		try {
+			File soundFile = null;
+			if (kind.equals("button")) {
+				// Open an audio input stream.
+				soundFile = new File("button.wav");
+			} else if (kind.equals("cheer")) {
+				// Open an audio input stream.
+				soundFile = new File("cheer.wav");
+			}
+			AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+			// Get a sound clip resource.
+			Clip clip = AudioSystem.getClip();
+			// Open audio clip and load samples from the audio input stream.
+			clip.open(audioIn);
+			clip.start();
+
+		} catch (UnsupportedAudioFileException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * this method performs the main game play of the Language Game
+	 * 
+	 * @param e
+	 */
+	public void actionPerformed(ActionEvent e) {
+		String text = ((AbstractButton) e.getSource()).getText();
+		sound("button");
+		// display different versions of game
+		if (text.equals("Word Game")) {
+			version = "Word Game";
+			logic = new GameLogic(version);
+			logic.updateWord();
+			removeAll();
+			createView();
+		} else if (text.equals("Recycle Game")) {
+			version = "Recycle Game";
+			logic = new GameLogic(version);
+			logic.updateWord();
+			removeAll();
+			createView();
+		} else if (text.equals("Skip")) {
+			logic.updateWord();
+			createView();
+		} else if (text.equals("End")) {
+			if (endGame())
+				sound("cheer");
+		} else if (text.equals("Restart")) {
+			removeAll();
+			displayVersion();
+		}
+		// compare selected category with the original
+		else {
+			correct = logic.compare(text);
+			if (logic.updateWord() == null) {
+				if (endGame())
+					sound("cheer");
+			} else {
+				gameStart = true;
+				// refresh display
+				createView();
+			}
+		}
+	}
+}
---
+++ Library.java
@@ -0,0 +1,158 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ * WordLibrary class reads words from external file and can generate a random
+ * word from the file
+ * 
+ * @author BluePuddleCat
+ * @version 11/3/18 Written for HackHolyoke 2018
+ *
+ */
+public class Library {
+
+	private LinkedList<Word> library = new LinkedList<Word>();
+
+	/**
+	 * Constructor reads file
+	 */
+	public Library(String version) {
+		try {
+			// Setup XML Document
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			if (version.equals("Word Game")) {
+				File xmlFile = new File("WordLibrary.xml");
+				Document document = builder.parse(xmlFile);
+				parseFounderFile(document);
+			} else if (version.equals("Recycle Game")) {
+				File xmlFile = new File("RecycleLibrary.xml");
+				Document document = builder.parse(xmlFile);
+				parseFounderFile(document);
+			}
+		}
+		// catch exceptions
+		catch (ParserConfigurationException pce) {
+			System.out.println("ParserConfigurationException found");
+		} catch (SAXException saxe) {
+			System.out.println("SAXException found");
+		} catch (IOException ioe) {
+			System.out.println("IOException found");
+		}
+	}
+
+	/**
+	 * Navigate through the major document structure
+	 * 
+	 * @param document:
+	 *            document to be parsed
+	 */
+	private void parseFounderFile(Document document) {
+		// get first node
+		Node docRoot = document.getDocumentElement();
+		parseNode(docRoot);
+	}
+
+	/**
+	 * Go through every node in the document
+	 * 
+	 * @param node:
+	 *            node in file
+	 */
+	private void parseNode(Node node) {
+
+		if (node.getNodeType() == Node.ELEMENT_NODE) {
+			if (node.getNodeName().equals("word_library")
+					|| node.getNodeName().equals("recycle_library") && node.hasChildNodes()) {
+				// for loop through all first-child nodes
+				NodeList nodeList = node.getChildNodes();
+				for (int i = 0; i < nodeList.getLength(); i++) {
+					Node firstChildNode = nodeList.item(i);
+					// check if child node is an element
+					if (firstChildNode.getNodeType() == Node.ELEMENT_NODE) {
+						// rename element node
+						Element currentElt = (Element) firstChildNode;
+						// check if the node is word
+						String nodeName = firstChildNode.getNodeName();
+						if (nodeName.equals("word") && firstChildNode.hasChildNodes()) {
+							Word newWord = new Word();
+							newWord.setCategory(currentElt.getAttribute("category"));
+							// for loop through second-child nodes
+							NodeList nodeList2 = firstChildNode.getChildNodes();
+							for (int j = 0; j < nodeList2.getLength(); j++) {
+								Node secondChildNode = nodeList2.item(j);
+								// check if child node is an element
+								if (secondChildNode.getNodeType() == Node.ELEMENT_NODE) {
+									// check if the node is name
+									String nodeName2 = secondChildNode.getNodeName();
+									if (nodeName2.equals("name")) {
+										newWord.setData(secondChildNode.getTextContent());
+										library.add(newWord);
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * get library
+	 * 
+	 * @return library
+	 */
+	public LinkedList<Word> getLibrary() {
+		return library;
+	}
+
+	/**
+	 * get library size
+	 * @return size
+	 */
+	public int getSize() {
+		return library.size();
+	}
+	
+	/**
+	 * get random word in the library
+	 * 
+	 * @return random word
+	 */
+	public Word getRandomWord() {
+		Word randomWord = null;
+		// check if the library is empty
+		if (library.size() == 0) {
+			System.out.println("No available word in the library.");
+		}
+		// if not empty
+		else {
+			int index = (int) ((library.size() - 1) * Math.random());
+			randomWord = library.get(index);
+			library.remove(index);
+		}
+		return randomWord;
+	}
+
+	/**
+	 * display every word in word library
+	 */
+	public String toString() {
+		String libraryToPrint = "";
+		for (int i = 0; i < library.size(); i++)
+			libraryToPrint += library.get(i).toString() + "\n";
+		return libraryToPrint;
+	}
+}
